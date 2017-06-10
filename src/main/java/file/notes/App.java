182a34d6706b337b7c0589d07032ae758b30f8ba
ygork <caruntuigora@gmail.com>
2017-06-10 15:04:52 +0300

    fix #1 Create command interface for basic commands

--- src/main/java/file/notes/App.java
+++ src/main/java/file/notes/App.java
@@ -1,10 +1,25 @@
 package file.notes;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String command = br.readLine();
+            System.out.println("Success: " + command);
+            if ("q".equals(command)) {
+                System.exit(0);
+            }
+
+
+        }
 
 
 

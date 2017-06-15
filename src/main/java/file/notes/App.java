9b56763e7c1c8cfa1645d050e7048a3ab6949928
ygork <caruntuigora@gmail.com>
2017-06-15 22:55:32 +0300

    Add constant

--- src/main/java/file/notes/App.java
+++ src/main/java/file/notes/App.java
@@ -4,10 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * Hello world!
- */
+
 public class App {
+    public static final String COMMAND_DELETE = "delete";
+    public static final String COMMAND_CREATE = "create";
+    public static final String COMMAND_SHOW = "show";
+    public static final String COMMAND_Q = "q";
+    public static final String COMMAND_ERROR = "Error. Unsupported command. Press one of commands: creade, delete, show.";
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -18,15 +21,15 @@ public class App {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            if ("q".equals(command)) {
+            if (COMMAND_Q.equals(command)) {
                 System.exit(0);
             }
-            else if ("create".equals(command) || "delete".equals(command) || "show".equals(command)) {
+            else if (COMMAND_CREATE.equals(command) || COMMAND_DELETE.equals(command) || COMMAND_SHOW.equals(command)) {
                 System.out.println("Success: " + command);
             }
-            else {
-                System.out.println("Error. Unsupported command. Press one of commands: creade, delete, show." );
+            else {System.out.println(COMMAND_ERROR );
             }
         }
+
     }
 }
